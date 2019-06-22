4458cc9afc0f82700be73444f888429f2a4bc1f8
HubCodes <krlrhkstk12@gmail.com>
2019-06-22 16:38:30 +0900

    Refactor Context.fromConfig

--- src/main/scala/context/Context.scala
+++ src/main/scala/context/Context.scala
@@ -9,15 +9,17 @@ case class Context(
                     registryURL: String,
                     userID: String,
                     password: String,
+                    runScripts: Map[String, String],
                     sleepFunction: SleepFunction,
-                    logger: Logger
+                    logger: Logger,
                   )
 
 object Context {
   def fromConfig(config: Config): Context = {
     val sleepFunction = (minute: Int) => Thread.sleep(minute.minutes.toMillis)
     val logger = Logger("Corgi")
+    val (registryURL, userID, password, _, runScripts) = Config.unapply(config).get
 
-    Context(config.registryURL, config.userID, config.password, sleepFunction, logger)
+    Context(registryURL, userID, password, runScripts, sleepFunction, logger)
   }
 }
