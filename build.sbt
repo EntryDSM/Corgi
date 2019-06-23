9cc117a85590f8ca7a4967d8eea6a6f0428e0523
HubCodes <krlrhkstk12@gmail.com>
2019-06-23 14:05:34 +0900

    Add fetch object for reusing authenticated request

--- build.sbt
+++ build.sbt
@@ -4,11 +4,13 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+resolvers += "Tim Tennant's repo" at "http://dl.bintray.com/timt/repo/"
+
 libraryDependencies ++= Seq(
   "org.scalaz" %% "scalaz-core" % "7.2.27",
   "org.scalaz" %% "scalaz-ioeffect" % "2.10.1",
   "org.json4s" %% "json4s-native" % "3.6.6",
-  "com.lihaoyi" %% "requests" % "0.2.0",
   "ch.qos.logback" % "logback-classic" % "1.2.3",
-  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
+  "io.shaka" %% "naive-http" % "104"
 )
--- src/main/scala/context/Context.scala
+++ src/main/scala/context/Context.scala
@@ -9,6 +9,7 @@ case class Context(
                     registryURL: String,
                     userID: String,
                     password: String,
+                    pollingRatePerMinute: Int,
                     runScripts: Map[String, String],
                     sleepFunction: SleepFunction,
                     logger: Logger,
@@ -18,8 +19,8 @@ object Context {
   def fromConfig(config: Config): Context = {
     val sleepFunction = (minute: Int) => Thread.sleep(minute.minutes.toMillis)
     val logger = Logger("Corgi")
-    val (registryURL, userID, password, _, runScripts) = Config.unapply(config).get
+    val (registryURL, userID, password, pollingRatePerMinute, runScripts) = Config.unapply(config).get
 
-    Context(registryURL, userID, password, runScripts, sleepFunction, logger)
+    Context(registryURL, userID, password, pollingRatePerMinute, runScripts, sleepFunction, logger)
   }
 }
