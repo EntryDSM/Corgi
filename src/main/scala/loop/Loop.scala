1c8cbb5528139d55e4d2b579188b7bd966df0925
HubCodes <krlrhkstk12@gmail.com>
2019-07-01 11:03:11 +0900

    Refactor Loop.scala
    * move codes for fetching registry information to package registry

--- src/main/scala/loop/Loop.scala
+++ src/main/scala/loop/Loop.scala
@@ -1,14 +1,6 @@
 package loop
 
 import context.Context
-import io.shaka.http.Http.http
-import io.shaka.http.{HttpHeader, Method}
-import org.json4s._
-import org.json4s.native.JsonMethods._
-import scalaz.std.list.listInstance
-import scalaz.std.option.optionInstance
-import scalaz.syntax.traverse.ToTraverseOps
-import util.Fetch
 
 object Loop {
 
@@ -19,45 +11,11 @@ object Loop {
   @scala.annotation.tailrec
   def loop(context: Context): Unit = {
     val registryImageDigestList = for {
-      registryImageList <- getRegistryImageList(context)
-      imageDigestList <- getRegistryImageDigestList(context, registryImageList)
+      registryImageList <- registry.getRegistryImageList(context)
+      imageDigestList <- registry.getRegistryImageDigestList(context, registryImageList)
     } yield (registryImageList zip imageDigestList).toMap
 
     context.sleepFunction(context.pollingRatePerMinute)
     loop(context)
   }
-
-  def getRegistryImageList(context: Context): Option[List[ImageName]] = {
-    val requestObject = Fetch.authenticatedRequest(context, Method.GET, pathForGetRegistryImageList)
-    val response = http(requestObject)
-    val body = for {
-      entity <- response.entity
-    } yield parse(entity.toString()).extract[RegistryImageList]
-
-    body map { registryImageList => registryImageList.repositories }
-  }
-
-  def getRegistryImageDigestList(context: Context, imageList: List[String]): Option[List[ImageDigest]] = {
-    val digests = imageList map { imageName =>
-      val requestObject = Fetch.authenticatedRequest(context, Method.HEAD, pathForGetRegistryDigest(imageName))
-      val response = http(requestObject)
-      val digest = response.header(HttpHeader.unknownHeader(dockerDigestHeaderFieldName))
-
-      digest
-    }
-    digests.sequence
-  }
-
-  implicit val formats: DefaultFormats = DefaultFormats
-
-  private def dockerDigestHeaderFieldName = "Docker-Content-Digest"
-
-  private def pathForGetRegistryImageList = "/v2/_catalog"
-
-  private def pathForGetRegistryDigest(imageName: String) = {
-    "v2/" + imageName + "/manifests/latest"
-  }
-
-  case class RegistryImageList(repositories: List[String])
-
 }
---
+++ src/main/scala/registry/registry.scala
@@ -0,0 +1,47 @@
+import context.Context
+import io.shaka.http.Http.http
+import io.shaka.http.{HttpHeader, Method}
+import loop.Loop.{ImageDigest, ImageName}
+import org.json4s.DefaultFormats
+import org.json4s.native.JsonMethods.parse
+import scalaz.std.list.listInstance
+import scalaz.std.option.optionInstance
+import scalaz.syntax.traverse.ToTraverseOps
+import util.Fetch
+
+package object registry {
+
+  def getRegistryImageList(context: Context): Option[List[ImageName]] = {
+    val requestObject = Fetch.authenticatedRequest(context, Method.GET, pathForGetRegistryImageList)
+    val response = http(requestObject)
+    val body = for {
+      entity <- response.entity
+    } yield parse(entity.toString()).extract[RegistryImageList]
+
+    body map { registryImageList => registryImageList.repositories }
+  }
+
+  private def pathForGetRegistryImageList = "/v2/_catalog"
+
+  implicit val formats: DefaultFormats = DefaultFormats
+
+  def getRegistryImageDigestList(context: Context, imageList: List[String]): Option[List[ImageDigest]] = {
+    val digests = imageList map { imageName =>
+      val requestObject = Fetch.authenticatedRequest(context, Method.HEAD, pathForGetRegistryDigest(imageName))
+      val response = http(requestObject)
+      val digest = response.header(HttpHeader.unknownHeader(dockerDigestHeaderFieldName))
+
+      digest
+    }
+    digests.sequence
+  }
+
+  private def dockerDigestHeaderFieldName = "Docker-Content-Digest"
+
+  private def pathForGetRegistryDigest(imageName: String) = {
+    "v2/" + imageName + "/manifests/latest"
+  }
+
+  case class RegistryImageList(repositories: List[String])
+
+}
