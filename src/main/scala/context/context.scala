bb02c4134d15c38c64cf93e90b07ace55e9d336e
HubCodes <krlrhkstk12@gmail.com>
2019-07-08 10:48:21 +0900

    Add Context case class
    * Environment of Corgi

--- src/main/scala/context/context.scala
+++ src/main/scala/context/context.scala
@@ -1,3 +1,36 @@
+import com.typesafe.scalalogging.Logger
+import config.Config
+
+import scala.concurrent.duration._
+
 package object context {
   type SleepFunction = Int => Unit
+
+  case class Context(
+                      registryURL: String,
+                      userID: String,
+                      password: String,
+                      pollingRatePerMinute: Int,
+                      runScripts: Map[String, String],
+                      sleepFunction: context.SleepFunction,
+                      logger: Logger,
+                      dockerEngineSocket: String
+                    )
+
+  object Context {
+    def fromConfig(config: Config): Context = {
+      val sleepFunction = (minute: Int) => Thread.sleep(minute.minutes.toMillis)
+      val logger = Logger("Corgi")
+      val (
+        registryURL,
+        userID,
+        password,
+        pollingRatePerMinute,
+        runScripts,
+        dockerEngineSocket
+        ) = Config.unapply(config).get
+
+      Context(registryURL, userID, password, pollingRatePerMinute, runScripts, sleepFunction, logger, dockerEngineSocket)
+    }
+  }
 }
