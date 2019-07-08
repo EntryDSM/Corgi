adf4a88cd16e5c588338ad4f011238ec6834ae54
HubCodes <krlrhkstk12@gmail.com>
2019-07-08 11:55:27 +0900

    Add checking logic that checks local image expired
    * It emits list of expired images

--- src/main/scala/loop/Loop.scala
+++ src/main/scala/loop/Loop.scala
@@ -1,6 +1,7 @@
 package loop
 
 import context.Context
+import docker.ImageDigest
 
 object Loop {
 
@@ -18,6 +19,19 @@ object Loop {
       imageDigestList <- docker.getImageDigestList(digestList)
       mapOfDuplicatedImageNameToDigest = duplicatedImageNameList zip imageDigestList
     } yield mapOfDuplicatedImageNameToDigest.groupBy(_._1).map { case (key, value) => (key, value.map(_._2)) }
+
+    def isLocalImageNewest(localImageDigestList: List[ImageDigest], remoteImageDigest: ImageDigest): Boolean = {
+      localImageDigestList contains remoteImageDigest
+    }
+
+    val needingUpdateImageList = newestImageDigestMap map { newestImageDigestMap =>
+      for {
+        (remoteKey, remoteValue) <- newestImageDigestMap
+        localImageDigestMap <- localImageDigestMap
+        localValue <- localImageDigestMap.get(remoteKey)
+        if !isLocalImageNewest(localValue, remoteValue)
+      } yield remoteKey
+    }
 
     context.sleepFunction(context.pollingRatePerMinute)
     loop(context)
