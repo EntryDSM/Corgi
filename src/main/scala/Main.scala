62fd3069eddd0aad7d0c9a4e9c2e476cbf0420bb
HubCodes <krlrhkstk12@gmail.com>
2019-07-13 10:23:51 +0900

    Add loop executing logic in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,17 @@
 import config.Config
 import context.Context
+import loop.Loop
 
 object Main {
   def main(args: Array[String]): Unit = {
     val context = for {
       config <- Config.fromArgs(args)
     } yield Context.fromConfig(config)
+
+    context match {
+      case Some(ctx) => Loop.loop(ctx)
+      case None =>
+        println("Cannot execute Corgi. please check configuration file.")
+    }
   }
 }
