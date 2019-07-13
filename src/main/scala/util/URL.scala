35c0d9feed34bdb15f1a316e1c2415bee2f81f61
HubCodes <krlrhkstk12@gmail.com>
2019-07-13 10:41:04 +0900

    Fix invalid pattern match

--- src/main/scala/util/URL.scala
+++ src/main/scala/util/URL.scala
@@ -5,7 +5,7 @@ import java.nio.file.Paths
 object URL {
   def join(urls: String*): String = {
     val joinedUrl = urls match {
-      case first :: more             => Some(Paths.get(first, more: _*))
+      case first +: more             => Some(Paths.get(first, more: _*))
       case first if urls.length == 1 => Some(Paths.get(first.head))
     }
     joinedUrl.toString
